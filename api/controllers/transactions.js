5d018946c2a3db92769320941981670ec367016e
Jose Montero <lbo2mail@gmail.com>
2020-03-05 15:50:40 -0300

    Se implementa el metodo getTransaccion

--- api/controllers/transactions.js
+++ api/controllers/transactions.js
@@ -3,20 +3,33 @@ const Transaction = require('../models/Transaction');
 // @desc        get all transactions
 // @route       GET /api/v1/transactions
 // @access      public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+        
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server error'
+        })
+    }
 }
 
 // @desc        add transaction
 // @route       POST /api/v1/transactions
 // @access      public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     res.send('POST transactions');
 }
 
 // @desc        delete transaction
 // @route       DELETE /api/v1/transactions/:id
 // @access      public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transaction');
 }
