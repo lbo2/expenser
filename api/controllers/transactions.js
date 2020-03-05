04515db19f367a17dfe7db7f25d75bc8fa5d9bfa
Jose Montero <lbo2mail@gmail.com>
2020-03-05 14:47:58 -0300

    se crea estructura de rutas y controladores

---
+++ api/controllers/transactions.js
@@ -0,0 +1,20 @@
+// @desc        get all transactions
+// @route       GET /api/v1/transactions
+// @access      public
+exports.getTransactions = (req, res, next) => {
+    res.send('GET transactions');
+}
+
+// @desc        add transaction
+// @route       POST /api/v1/transactions
+// @access      public
+exports.addTransaction = (req, res, next) => {
+    res.send('POST transactions');
+}
+
+// @desc        delete transaction
+// @route       DELETE /api/v1/transactions/:id
+// @access      public
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE transaction');
+}
